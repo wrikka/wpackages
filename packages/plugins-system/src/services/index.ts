d27fa188a80826bbc0e08c2a42c36db51d90acd3
Veerapong <newkubise@gmail.com>
2026-01-10 20:25:59 +0700

    refactor(plugins-system): update index exports

--- packages/plugins-system/src/services/index.ts
+++ packages/plugins-system/src/services/index.ts
@@ -4,3 +4,19 @@ export { loadPlugin, loadPlugins, type PluginLoadResult, sortPluginsByDependenci
 export { createPluginManager, type PluginManager } from "./plugin-manager.service";
 export { analyzeRegistry, createMetricsCollector, type MetricsCollector } from "./plugin-metrics.service";
 export { createFileStorage, createMemoryStorage, type PluginStorage } from "./plugin-storage.service";
+
+export { createHotReloadManager } from "./hot-reload.service";
+export { createSandboxManager } from "./sandbox.service";
+export { createMarketplaceManager } from "./marketplace.service";
+export { createConfigSchemaManager } from "./config-schema.service";
+export { createPermissionManager } from "./permissions.service";
+export { createPerformanceMonitor } from "./performance.service";
+export { createHealthCheckManager } from "./health-check.service";
+export { createCacheManager } from "./cache.service";
+export { createTelemetryManager } from "./telemetry.service";
+export { createTestingFramework } from "./testing.service";
+export { createDocumentationGenerator } from "./documentation.service";
+export { createCLITool } from "./cli.service";
+export { createMigrationManager } from "./migration.service";
+export { createServiceContainer, createDIContext, createDIPluginAPI } from "./di.service";
+export { createAdvancedEventBus } from "./event-bus.service";
--- packages/plugins-system/src/types/index.ts
+++ packages/plugins-system/src/types/index.ts
@@ -26,6 +26,120 @@ export type {
 	PluginState,
 	PluginStatus,
 } from "./plugin.types";
-// Result type for plugin operations
+
 export type { PluginResult } from "./result";
 export { success, failure, isSuccess, isFailure, mapResult, flatMapResult, getOrThrow, getOrDefault } from "./result";
+
+export type {
+	HotReloadEvent,
+	HotReloadManager,
+	HotReloadOptions,
+	HotReloadResult,
+	HotReloadState,
+	HotReloadStrategy,
+} from "./hot-reload.types";
+
+export type {
+	SandboxContext,
+	SandboxManager,
+	SandboxOptions,
+	SandboxResult,
+	SandboxType,
+	ResourceLimit,
+	ResourceLimitType,
+} from "./sandbox.types";
+
+export type {
+	MarketplaceDependency,
+	MarketplaceInstallOptions,
+	MarketplaceManager,
+	MarketplacePlugin,
+	MarketplaceSearchOptions,
+	MarketplaceSearchResult,
+} from "./marketplace.types";
+
+export type {
+	ConfigMigration,
+	ConfigSchema,
+	ConfigSchemaManager,
+	ConfigValidationError,
+	ConfigValidationResult,
+	SchemaProperty,
+} from "./config-schema.types";
+
+export type {
+	HealthCheckConfig,
+	HealthCheckManager,
+	HealthCheckOptions,
+	HealthCheckResult,
+	HealthStatus,
+} from "./health-check.types";
+
+export type {
+	CacheEntry,
+	CacheManager,
+	CacheOptions,
+	CacheStats,
+	CacheStrategy,
+} from "./cache.types";
+
+export type {
+	TelemetryConfig,
+	TelemetryEvent,
+	TelemetryManager,
+	UsageStats,
+	FeatureUsage,
+} from "./telemetry.types";
+
+export type {
+	TestFixture,
+	TestCase,
+	TestSuite,
+	TestResult,
+	TestRunResult,
+	TestingFramework,
+} from "./testing.types";
+
+export type {
+	DocumentationOptions,
+	APIDocumentation,
+	MethodDocumentation,
+	ParameterDocumentation,
+	EventDocumentation,
+	TypeDocumentation,
+	PropertyDocumentation,
+	DocumentationGenerator,
+} from "./documentation.types";
+
+export type {
+	CLICommand,
+	CLIOption,
+	CLIConfig,
+	CLIRunResult,
+	CLITool,
+} from "./cli.types";
+
+export type {
+	MigrationScript,
+	MigrationResult,
+	MigrationHistory,
+	MigrationOptions,
+	MigrationManager,
+} from "./migration.types";
+
+export type {
+	DependencyDescriptor,
+	DependencyLifetime,
+	ServiceContainer,
+	DIContext,
+	DIPluginAPI,
+} from "./di.types";
+
+export type {
+	EventFilter,
+	EventTransformer,
+	EventSubscription,
+	EventReplayOptions,
+	PersistedEvent,
+	AdvancedEventBus,
+} from "./event-bus.types";
