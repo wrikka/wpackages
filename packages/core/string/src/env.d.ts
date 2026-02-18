06a84f25f822c05e24f70d18af36c446b6c51693
Veerapong <newkubise@gmail.com>
2026-02-19 01:24:07 +0700

    chore: remove unused core packages
    - Remove packages/core/cli
    - Remove packages/core/scripts
    - Remove packages/core/string
    - Remove packages/tasks
    - Remove packages/vitext

--- packages/core/string/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-export {};
-
-declare global {
-	interface Env {
-		TZ?: string;
-	}
-}
--- packages/core/string/src/string-helper.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { capitalize } from "./string-helper";
-
-test("capitalize", () => {
-	expect(capitalize("hello")).toBe("Hello");
-	expect(capitalize("WORLD")).toBe("WORLD");
-	expect(capitalize("")).toBe("");
-});
