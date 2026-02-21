a041fbef329288fe70c699f2c5358daa15985de3
Veerapong <newkubise@gmail.com>
2026-02-21 21:22:47 +0700

    Remove config files: biome.jsonc, dprint.json, vitest.workspace.ts, wtest.config.ts, .oxlintrc.json

--- wtest.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "@wpackages/test/config";
-
-export default defineConfig({
-	globals: true,
-	include: ["**/*.wtest.ts"],
-	exclude: ["**/node_modules/**", "**/dist/**"],
-});
